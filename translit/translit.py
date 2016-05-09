d85c860d0ce7ac42597fb50cb14e695f9d681570
Patrik Hagara <phagara@redhat.com>
2016-05-09 19:15:36 +0200

    add translit.py

---
+++ translit/translit.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import transliterate
+
+
+class Translit:
+    def __init__(self, bot):
+        self.bot = bot
+        self.avail = transliterate.get_available_language_codes()
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def translit(self, ctx, target: str, *text: str):
+        if target not in self.avail:
+            await self.bot.reply("Unknown target language charset code. "
+                                 "Supported: {}".format(", ".join(self.avail)))
+        else:
+            text = " ".join(text)
+            await self.bot.reply(transliterate.translit(text, target))
+
+
+def setup(bot):
+    bot.add_cog(Translit(bot))
