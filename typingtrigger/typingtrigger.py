a8a5a24b58e46382e7bb1c224371340223004db8
Patrik Hagara <phagara@redhat.com>
2016-10-31 19:27:40 +0100

    uncached the cache for better or worse

--- typingtrigger/typingtrigger.py
+++ typingtrigger/typingtrigger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import logging
+import datetime
 import discord
 from discord.ext import commands
 from cogs.utils import checks
@@ -9,6 +10,24 @@ from __main__ import send_cmd_help
 
 
 log = logging.getLogger('red.TypingTrigger')
+
+
+class UnCache(object):
+    def __init__(self, ttl=datetime.timedelta(seconds=10)):
+        self.bag = []
+        self.ttl = ttl
+
+    def add(self, item):
+        self.bag.append((
+            item,
+            datetime.datetime.utcnow() + self.ttl
+        ))
+        return len(self)
+
+    def __len__(self):
+        self.bag = [item for item in self.bag
+                    if item[1] >= datetime.datetime.utcnow()]
+        return len(set([item[0] for item in self.bag]))
 
 
 class TypingTrigger:
@@ -22,6 +41,7 @@ class TypingTrigger:
                 '/me typing',
             ],
         }
+        self.bagsofdicks = {}
 
     # TODO: command to add new triggers
 
@@ -37,8 +57,13 @@ class TypingTrigger:
         else:
             await self.bot.send_typing(message.channel)
 
+    async def handle_typing(self, channel, user, when):
+        if self.bagsofdicks.setdefault(channel.name, UnCache()).add(user.name) >= 3:
+            await self.bot.send_typing(message.channel)
+
 
 def setup(bot):
     typingtrigger = TypingTrigger(bot)
     bot.add_listener(typingtrigger.handle_message, 'on_message')
+    bot.add_listener(typingtrigger.handle_typing, 'on_typing')
     bot.add_cog(typingtrigger)
